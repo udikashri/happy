9f38c3226afcfc97316e32fafdaeee9ed506d875
Tzvika Avnery <tzvik2000@gmail.om>
2021-01-20 20:17:10 +0200

    seller backoffice design

--- front/src/cmps/ItemEdit.jsx
+++ front/src/cmps/ItemEdit.jsx
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import { saveItem } from '../store/actions/itemActions.js'
 import { Uploader } from './Uploader';
+import NotInterestedIcon from '@material-ui/icons/NotInterested';
 
 
 export class _ItemEdit extends Component {
@@ -97,10 +98,24 @@ export class _ItemEdit extends Component {
       <section className="edit-box">
         <form onSubmit={(event) => { this.onSaveItem(event) }}>
           <input autoFocus type="text" value={currItem.title} onChange={this.handleInput} name="title" placeholder="Add Item" autoComplete="off" />
-          <input autoFocus type="text" value={currItem.color} onChange={this.handleInput} name="color" placeholder="Add Color" autoComplete="off" />
-          {/* <input autoFocus type="text" value={currItem.type} onChange={this.handleInput} name="type" placeholder="Add Type" autoComplete="off" /> */}
+                    {/* <input autoFocus type="text" value={currItem.type} onChange={this.handleInput} name="type" placeholder="Add Type" autoComplete="off" /> */}
           <textarea rows="5" autoFocus type="text" value={currItem.description} onChange={this.handleInput} name="description" placeholder="Add Description" autoComplete="off" />
           <label >Price: <input type="number" min="0" name="price" value={currItem.price} onChange={this.handleInput} /></label>
+          <div className="color-container">
+                    <section title="Red" onClick={() => {this.handleChangeColor('red')}} className="red"></section>
+                    <section title="Gray" onClick={() => {this.handleChangeColor('gray')}} className="gray"></section>
+                    <section title="Blue" onClick={() => {this.handleChangeColor('blue')}} className="blue"></section>
+                    <section title="Pink" onClick={() => {this.handleChangeColor('pink')}} className="pink"></section>
+                    <section title="Yellow" onClick={() => {this.handleChangeColor('yellow')}} className="yellow"></section>
+                    <section title="White" onClick={() => {this.handleChangeColor('white')}} className="white"></section>
+                    <section title="Black" onClick={() => {this.handleChangeColor('black')}} className="black"></section>
+                    <section title="Green" onClick={() => {this.handleChangeColor('green')}} className="green"></section>
+                    <section title="Purple" onClick={() => {this.handleChangeColor('purple')}} className="purple"></section>
+                    <section title="Brown" onClick={() => {this.handleChangeColor('brown')}} className="brown"></section>
+                    <section title="colorful" onClick={() => {this.handleChangeColor('colorful')}} className="colorful"></section>
+                    <section title="Clear" onClick={() => {this.handleChangeColor('clear')}} className="all"><NotInterestedIcon/></section>
+
+                </div>
           <Uploader onFinishUpload={this.onUploadItemImage} />
           <button>Save</button>
         </form>
