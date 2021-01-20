b7784782f149c77f8a005e1e71afec8d1a1a1de6
Hadar.Gasner <HadarGasner@gmail.com>
2021-01-20 17:10:38 +0200

    add thank

--- front/src/pages/ThankYou.jsx
+++ front/src/pages/ThankYou.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function ThankYou(props) {
     const { item } = props;
     return <section className="thank-you">
-        thank you next
+    <div className="thank-msg">
+        <div>the socks is on the way</div>
+        <Link to={'/shop'}>continue shoping</Link>
+    </div>
 
     <div class="speech half">thank you Yaron</div>
         <img src="https://res.cloudinary.com/dt1zahrqy/image/upload/v1611052878/atmosphere/3485-removebg-preview_1_gy43es.png" />
