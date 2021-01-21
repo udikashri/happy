1275d143b7c64efaa38e12d3525db3167d8910a0
udikashri <udikashri@gmail.com>
2021-01-22 01:12:09 +0200

    enter

--- front/src/cmps/ItemEdit.jsx
+++ front/src/cmps/ItemEdit.jsx
@@ -34,17 +34,21 @@ export class _ItemEdit extends Component {
   componentDidMount() {
     const currItem  =   {
         // type: "",
-        title: "",
+        title: '',
         price: 0,
-        color: "",
+        color: '',
         seller: {
+          _id:'E4G3Ck',
+          fullname:'Amelia Larson',
+          imgUrl:'https://res.cloudinary.com/dt1zahrqy/image/upload/v1610610532/hf/faces/3_nz6vkf.jpg'
         },
-        description: "",
-        imgUrl: "https://ih1.redbubble.net/image.951124242.1192/ur,socks_flatlay_medium,square,600x600-bg,f8f8f8.1.jpg",
+        description: '',
+        imgUrl: 'https://ih1.redbubble.net/image.951124242.1192/ur,socks_flatlay_medium,square,600x600-bg,f8f8f8.1.jpg',
         tags: [
-          "one",
-          "tow"
-        ]
+          ''
+        ],
+        likes:'',
+        details:''
       }
     this.setState({currItem})
 
@@ -53,9 +57,9 @@ export class _ItemEdit extends Component {
   onSaveItem = async (ev) => {
     ev.preventDefault()
     const  item = this.state.currItem
-    item.seller._id = this.props.user._id
-    item.seller.fullname = this.props.user.fullname
-    item.seller.imgUrl = this.props.user.imgUrl
+    // item.seller._id = this.props.user._id
+    // item.seller.fullname = this.props.user.fullname
+    // item.seller.imgUrl = this.props.user.imgUrl
     await this.props.saveItem(item)
     this.props.loadUserItems()
   }
@@ -70,6 +74,18 @@ export class _ItemEdit extends Component {
 
   handleInput = ({ target }) => {
     const { name, value } = target
+    this.setState(prevState => {
+      return {
+        currItem: {
+          ...prevState.currItem,
+          [name]: value
+        }
+      }
+    })
+  }
+  handleTagsInput = ({ target }) => {
+    var { name, value } = target
+    console.log(    value.split(','))
     this.setState(prevState => {
       return {
         currItem: {
@@ -105,6 +121,12 @@ export class _ItemEdit extends Component {
       <section className="edit-box">
         <form onSubmit={(event) => { this.onSaveItem(event) }}>
           <input autoFocus type="text" value={currItem.title} onChange={this.handleInput} name="title" placeholder="Add Item" autoComplete="off" />
+          <input autoFocus type="text" value={currItem.tags} onChange={this.handleTagsInput} name="tags" placeholder="Animal,Art" autoComplete="off" />
+          <input autoFocus type="text" value={currItem.seller._id} onChange={this.handleInput} name="_id" placeholder="sellerId" autoComplete="off" />
+          <input autoFocus type="text" value={currItem.seller.fullname} onChange={this.handleInput} name="fullname" placeholder="sellerFullname" autoComplete="off" />
+          <input autoFocus type="text" value={currItem.seller.imgUrl} onChange={this.handleInput} name="imgUrl" placeholder="sellerImgUrl" autoComplete="off" />
+          <input autoFocus type="text" value={currItem.likes} onChange={this.handleInput} name="likes" placeholder="Likes" autoComplete="off" />
+          <input autoFocus type="text" value={currItem.details} onChange={this.handleInput} name="details" placeholder="Details" autoComplete="off" />
                     {/* <input autoFocus type="text" value={currItem.type} onChange={this.handleInput} name="type" placeholder="Add Type" autoComplete="off" /> */}
           <textarea rows="5" autoFocus type="text" value={currItem.description} onChange={this.handleInput} name="description" placeholder="Add Description" autoComplete="off" />
           <label >Price: <input type="number" min="0" name="price" value={currItem.price} onChange={this.handleInput} /></label>
