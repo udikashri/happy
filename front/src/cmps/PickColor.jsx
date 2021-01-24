b0a5c8b118194b11b21c2098aa42f12cb43acdc0
Hadar.Gasner <HadarGasner@gmail.com>
2021-01-24 15:11:23 +0200

    color handle

--- front/src/cmps/PickColor.jsx
+++ front/src/cmps/PickColor.jsx
@@ -5,33 +5,70 @@ import NotInterestedIcon from '@material-ui/icons/NotInterested';
 
 export class PickColor extends Component {
     state = {
-        pickeColor: null
+        pickeColor: "all"
     }
 
     handlePickeColor = (color) => {
-        this.setState({pickeColor:c})
+        this.setState({ pickeColor: color })
     }
 
 
     render() {
-        const {pickeColor} = this.state
+        const { pickeColor } = this.state
         return (
             <div className="color-container">
                 <section title="Red" onClick={() => {
-                        this.props.handleChangeColor('red')
-                        // this.handlePickeColor('red')
-                }} className="red-fill"></section>
-                <section title="Gray" onClick={() => { this.props.handleChangeColor('gray') }} className="gray-fill"></section>
-                <section title="Blue" onClick={() => { this.props.handleChangeColor('blue') }} className="blue-fill"></section>
-                <section title="Pink" onClick={() => { this.props.handleChangeColor('pink') }} className="pink-fill"></section>
-                <section title="Yellow" onClick={() => { this.props.handleChangeColor('yellow') }} className="yellow-fill"></section>
-                <section title="White" onClick={() => { this.props.handleChangeColor('white') }} className="white-fill"></section>
-                <section title="Black" onClick={() => { this.props.handleChangeColor('black') }} className="black-fill"></section>
-                <section title="Green" onClick={() => { this.props.handleChangeColor('green') }} className="green-fill"></section>
-                <section title="Purple" onClick={() => { this.props.handleChangeColor('purple') }} className="purple-fill"></section>
-                <section title="Brown" onClick={() => { this.props.handleChangeColor('brown') }} className="brown-fill"></section>
-                <section title="colorful" onClick={() => { this.props.handleChangeColor('colorful') }} className="colorful"></section>
-                <section title="Clear" onClick={() => { this.props.handleChangeColor('clear') }} className="all"><NotInterestedIcon /></section>
+                    this.props.handleChangeColor('red')
+                    this.handlePickeColor('red')
+                }} className={(pickeColor === "red" || pickeColor === "all") ? "red-fill" : "red-border"} >
+                </section>
+                <section title="Gray" onClick={() => {
+                    this.props.handleChangeColor('gray')
+                    this.handlePickeColor('gray')
+                }} className={(pickeColor === "gray" || pickeColor === "all") ? "gray-fill" : "gray-border"} ></section>
+                <section title="Blue" onClick={() => {
+                    this.props.handleChangeColor('blue')
+                    this.handlePickeColor('blue')
+
+                }} className={(pickeColor === "blue" || pickeColor === "all") ? "blue-fill" : "blue-border"} ></section>
+                <section title="Pink" onClick={() => {
+                    this.props.handleChangeColor('pink')
+                    this.handlePickeColor('pink')
+
+                }} className={(pickeColor === "pink" || pickeColor === "all") ? "pink-fill" : "pink-border"} ></section>
+                <section title="Yellow" onClick={() => {
+                    this.handlePickeColor('yellow')
+                    this.props.handleChangeColor('yellow')
+                }} className={(pickeColor === "yellow" || pickeColor === "all") ? "yellow-fill" : "yellow-border"} ></section>
+                <section title="White" onClick={() => {
+                    this.props.handleChangeColor('white')
+                    this.handlePickeColor('white')
+
+                }} className={(pickeColor === "white" || pickeColor === "all") ? "white-fill" : "white-border"} ></section>
+                <section title="Black" onClick={() => {
+                    this.handlePickeColor('black')
+                    this.props.handleChangeColor('black')
+                }} className={(pickeColor === "black" || pickeColor === "all") ? "black-fill" : "black-border"} ></section>
+                <section title="Green" onClick={() => {
+                    this.handlePickeColor('green')
+                    this.props.handleChangeColor('green')
+                }} className={(pickeColor === "green" || pickeColor === "all") ? "green-fill" : "green-border"} ></section>
+                <section title="Purple" onClick={() => {
+                    this.handlePickeColor('purple')
+                    this.props.handleChangeColor('purple')
+                }} className={(pickeColor === "purple" || pickeColor === "all") ? "purple-fill" : "purple-border"} ></section>
+                <section title="Brown" onClick={() => {
+                    this.handlePickeColor('brown')
+                    this.props.handleChangeColor('brown')
+                }} className={(pickeColor === "brown" || pickeColor === "all") ? "brown-fill" : "brown-border"} ></section>
+                <section title="colorful" onClick={() => { 
+                    this.handlePickeColor('colorful')
+                    this.props.handleChangeColor('colorful') 
+                    }} className={(pickeColor === "colorful" || pickeColor === "all") ? "colorful-fill" : "colorful-border"}></section>
+                <section title="Clear" onClick={() => {
+                    this.handlePickeColor('all')
+                    this.props.handleChangeColor('clear')
+                }} className="all"><NotInterestedIcon /></section>
             </div>
         )
     }
