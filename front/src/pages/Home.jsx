0c7444d01f0152525744a1b8113c4f21c9567a56
udikashri <udikashri@gmail.com>
2021-01-24 15:26:46 +0200

    udi

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -163,6 +163,7 @@ class _Home extends Component {
               <div className="gallery">
                 <figure className="gallery__item gallery__item--1">
                   <img src="https://res.cloudinary.com/dt1zahrqy/image/upload/v1611301257/atmosphere/spo_dfghzs.jpg" alt="Gallery image 1" className="gallery__img" />
+                  <span className="gallery-tag-featured">Featured</span> 
                 </figure>
                 <figure className="gallery__item gallery__item--2">
                   <img src="https://res.cloudinary.com/dt1zahrqy/image/upload/v1611301259/atmosphere/80_vwru5z.png" alt="Gallery image 2" className="gallery__img" />
